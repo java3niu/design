564fc2ba07e76e31b3e468329e60431186955844
sanniu <1296646584@qq.com>
2022-08-22 23:14:16 +0800

    first commit

---
+++ src/test/java/site/sanniu/DesignApplicationTests.java
@@ -0,0 +1,13 @@
+package site.sanniu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
